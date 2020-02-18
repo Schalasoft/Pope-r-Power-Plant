06b25d82ab75e23772949308d0d959d276380160
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-02-18 17:22:31 +0000

    Initial commit

---
+++ Pope'r Power Plant/Pope'r Power Plant.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using STRINGS;
+using System;
+using static RoomConstraints;
+
+namespace PoperPowerPlant
+{
+    // Attach to RoomTypes constructor
+    [HarmonyPatch(typeof(Database.RoomTypes), MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { typeof(ResourceSet) })]
+    internal class PoperGenerator_RoomTypes
+    {
+        public static void Postfix(ref Database.RoomTypes __instance)
+        {
+            //Debug.Log(" === PoperPowerPlant_RoomTypes Postfix === ");
+
+            // Get Power Plant
+            RoomType roomType = __instance.TryGet("PowerPlant");
+
+            // Custom max size constraint
+            int customMaxSize = 240;
+            Constraint MaxSizeConstraint = new Constraint(null, (Room room) => room.cavity.numCells <= customMaxSize,
+                1,
+                string.Format(ROOMS.CRITERIA.MAXIMUM_SIZE.NAME, "" + customMaxSize),
+                string.Format(ROOMS.CRITERIA.MAXIMUM_SIZE.DESCRIPTION, "" + customMaxSize),
+                null);
+
+            // Iterate constraints
+            RoomConstraints.Constraint[] additional_constraints = roomType.additional_constraints;
+            for (int i = 0; i < additional_constraints.Length; i++)
+            {
+                // Change max size constraint
+                if (additional_constraints[i].name.Contains("Maximum size:"))
+                {
+                    additional_constraints[i] = MaxSizeConstraint;
+                }
+            }
+        }
+    }
+}
